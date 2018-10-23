87983d600743a56f3d17658d213d34003485d23d
aitbali <aitbali@hotmail.com>
2018-10-23 13:24:23 +0200

    start controller test

--- spec/controllers/turtles_controller_spec.rb
+++ spec/controllers/turtles_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe TurtlesController, type: :controller do
-
+  describe "#create"
+  describe "#index"
+  describe "#show"
+  describe "#update"
+  describe "#destroy"
 end
